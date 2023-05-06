feee1cd494d0be260bd55d23453d3c051d45d81f
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-05-06 12:35:26 -0400

    added shapes.js for classes

--- tests/svgUtils.test.js
+++ tests/svgUtils.test.js
@@ -1,4 +1,4 @@
-const { createSVG } = require('./lib/svgUtils');
+const { createSVG } = require('../lib/svgUtils');
 
 describe('createSVG', () => {
   test('should generate the correct SVG template', () => {
@@ -7,15 +7,13 @@ describe('createSVG', () => {
     const shape = 'circle';
     const shapeColor = 'blue';
 
-    const expectedSVG = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-        <rect width="100%" height="100%" />
-        <circle cx="300" cy="200" r="100" fill="blue" />
-        <text x="50%" y="52.5%" font-size="48" text-anchor="middle" fill="red">
-          ABC
-        </text>
-      </svg>
-    `;
+const expectedSVG = `
+<svg xmlns="http://www.w3.org/2000/svg" width="600" height="400">
+<circle cx="300" cy="200" r="100" fill="blue" />
+<text x="50%" y="52.5%" font-size="48" text-anchor="middle" fill="red">
+ABC
+</text>
+</svg>`;
 
     const generatedSVG = createSVG(text, textColor, shape, shapeColor);
     expect(generatedSVG).toEqual(expectedSVG);
